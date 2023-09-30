4ec7573c38cb9e8dabd68b8437e0e95259b354a8
pablo-ambrogio <pabloambrogioc@gmail.com>
2023-09-30 00:01:39 -0300

    Primer commit

---
+++ src/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'mi primer tarea',
+        description: "mi primer tarea",
+    },
+    {
+        id: 1,
+        title: 'mi primer segunda',
+        description: "mi primer segunda",
+    },
+    {
+        id: 2,
+        title: 'mi primer tercer',
+        description: "mi primer tercer",
+    }
+]
